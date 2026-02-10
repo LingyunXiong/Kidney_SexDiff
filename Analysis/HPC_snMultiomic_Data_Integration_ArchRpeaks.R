96144216ce3e060a86b735e8291676c6fb481705
Ivy Xiong <74974567+LingyunXiong@users.noreply.github.com>
2026-02-10 13:37:28 -0800

    Update

--- Analysis/HPC_snMultiomic_Data_Integration_ArchRpeaks.R
+++ Analysis/HPC_snMultiomic_Data_Integration_ArchRpeaks.R
@@ -10,7 +10,7 @@ library(RColorBrewer)
 my_cols = brewer.pal(4,"Spectral")
 
 #### Download processed single-nuclear multiomic dataset ####
-# Link:https://drive.google.com/file/d/1G8dC-bEj8UiXODtNkGKm7LwtO1n5xz9T/view?usp=sharing
+# Link: https://ucla.box.com/s/6f43jpkv5sco1bmcv8ttmp19edz9pnaw
 
 ### Load merged object ###
 Merged_f2 <- readRDS("Integrated_2Sex_ArchRpeaks_Filtered.rds")
